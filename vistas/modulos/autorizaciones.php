461bb5bfaf65abc56aca4d0c9e077bafdf04cae0
Brayan <braiancamilo0@gmail.com>
2025-05-16 17:41:37 -0500

    Burnt modal data removed.

--- vistas/modulos/autorizaciones.php
+++ vistas/modulos/autorizaciones.php
@@ -47,6 +47,10 @@
                         <input type="checkbox" id="auth1_2">
                         <label for="auth1_2"></label>
                       </div>
+                      <div class="icheck-primary d-inline">
+                        <input type="checkbox" id="auth1_3">
+                        <label for="auth1_3"></label>
+                      </div>
                     </td>
                     <td>
                       <button class="btn btn-info btn-sm" data-toggle="modal" data-target="#detailsModal1">Ver</button>
@@ -66,6 +70,10 @@
                         <input type="checkbox" id="auth2_2">
                         <label for="auth2_2"></label>
                       </div>
+                      <div class="icheck-primary d-inline">
+                        <input type="checkbox" id="auth2_3">
+                        <label for="auth2_3"></label>
+                      </div>
                     </td>
                     <td>
                       <button class="btn btn-info btn-sm" data-toggle="modal" data-target="#detailsModal2">Ver</button>
@@ -84,18 +92,18 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="detailsModalLabel1">Detalles de Cristian Orozco</h5>
+            <h5 class="modal-title" id="detailsModalLabel1"></h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
             <table class="table table-bordered">
-              <tr><td>Tipo de usuario</td><td>Instructor</td></tr>
-              <tr><td>Fecha de reserva</td><td>10/2000</td></tr>
-              <tr><td>Fecha de entrega</td><td>10/2000/09</td></tr>
-              <tr><td>Objeto</td><td>Equipo</td></tr>
-              <tr><td>Cantidad</td><td>04</td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
             </table>
           </div>
           <div class="modal-footer">
@@ -113,18 +121,18 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="detailsModalLabel2">Detalles de Luis Hernei</h5>
+            <h5 class="modal-title" id="detailsModalLabel2"></h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
             <table class="table table-bordered">
-              <tr><td>Tipo de usuario</td><td>Instructor</td></tr>
-              <tr><td>Fecha de reserva</td><td>10/2000/09</td></tr>
-              <tr><td>Fecha de entrega</td><td>10/2000/09</td></tr>
-              <tr><td>Objeto</td><td>Equipo</td></tr>
-              <tr><td>Cantidad</td><td>04</td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
+              <tr><td></td><td></td></tr>
             </table>
           </div>
           <div class="modal-footer">
