7d5f06d5233d60d51acfd9586ef819bed22d1728
Juan Pablo Montaño <jotapemontano13@gmail.com>
2025-05-19 09:01:04 -0500

    ultimo cambio para agregar equipos

--- controladores/equipos.controlador.php
+++ controladores/equipos.controlador.php
@@ -14,7 +14,7 @@ class ControladorEquipos{
     public static function ctrAgregarEquipos(){
         if (isset($_POST["numero_serie"]) && isset($_POST["etiqueta"]) && isset($_POST["descripcion"]) && isset($_POST["ubicacion_id"]) && isset($_POST["categoria_id"]) && isset($_POST["cuentadante_id"])) {
             // Faltaban los preg match
-            if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["numero_serie"]) && preg_match('/^[a-zA-Z0-9]+$/', $_POST["etiqueta"]) && preg_match('/^[a-zA-ZñÑáéíóÁÉÍÓÚ ]+$/', $_POST["descripcion"])) {
+            if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["numero_serie"]) && preg_match('/^[a-zA-Z0-9]+$/', $_POST["etiqueta"]) && preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["descripcion"])) {
                 // Mostrar datos antes de enviarlos al modelo
                 $tabla = "equipos";
                 $datos = array(
