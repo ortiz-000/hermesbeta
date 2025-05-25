d3e66b09933760757c720b2c812dfd1166c2bd84
Brayan <braiancamilo0@gmail.com>
2025-05-24 21:35:14 -0500

    Merging stuff.

---
+++ ajax/autorizaciones.ajax.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once "../controladores/autorizaciones.controlador.php";
+require_once "../modelos/autorizaciones.modelo.php";
+
+class AjaxAutorizaciones
+{
+    /*=============================================
+    VARIABLES
+    =============================================*/
+    public $idAutorizacion;
+    public $estado;
+
+    /*=============================================
+    MOSTRAR DETALLES DE AUTORIZACIÓN
+    =============================================*/
+    public function ajaxMostrarAutorizacion()
+    {
+        $item = "id_autorizacion";
+        $valor = $this->idAutorizacion;
+
+        $respuesta = ControladorAutorizaciones::ctrMostrarAutorizaciones($item, $valor);
+        echo json_encode($respuesta);
+    }
+
+    /*=============================================
+    ACTUALIZAR ESTADO DE AUTORIZACIÓN
+    =============================================*/
+    public function ajaxActualizarAutorizacion()
+    {
+        $datos = array(
+            "id_autorizacion" => $this->idAutorizacion,
+            "estado" => $this->estado
+        );
+
+        $controlador = new ControladorAutorizaciones();
+        $respuesta = $controlador->ctrActualizarAutorizacion($datos);
+        echo json_encode($respuesta);
+    }
+}
+
+/*=============================================
+MOSTRAR DETALLES DE AUTORIZACIÓN
+=============================================*/
+if (isset($_POST["idAutorizacion"]) && !isset($_POST["estado"])) {
+    $autorizar = new AjaxAutorizaciones();
+    $autorizar->idAutorizacion = $_POST["idAutorizacion"];
+    $autorizar->ajaxMostrarAutorizacion();
+}
+
+/*=============================================
+ACTUALIZAR ESTADO DE AUTORIZACIÓN
+=============================================*/
+if (isset($_POST["idAutorizacion"]) && isset($_POST["estado"])) {
+    $actualizar = new AjaxAutorizaciones();
+    $actualizar->idAutorizacion = $_POST["idAutorizacion"];
+    $actualizar->estado = $_POST["estado"];
+    $actualizar->ajaxActualizarAutorizacion();
+}
