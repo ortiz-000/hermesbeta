148b8dd71b6efb014df95a2d3d89edba9bac74a5
Cristian Gomez <ycamigo123@gmail.com>
2025-05-29 13:35:48 -0500

    refactor(conexion): update database connection parameters
    Change database name from 'hermes002' to 'pruebas' and remove password for development environment. This modification prepares the connection for testing purposes.

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,9 +1,8 @@
 <?php
 
 class Conexion{
-
     static public function conectar(){
-        $link = new PDO("mysql:host=localhost;dbname=hermes002", "root", "root");
+        $link = new PDO("mysql:host=localhost;dbname=pruebas", "root", "");
         $link -> exec("set names utf8");
         return $link;
     }
