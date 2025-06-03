fdf9bae27ddd0c7e97b3649e5a595b308ce81f75
sebastian <ortizjuansebas05@gmail.com>
2025-06-03 17:44:26 -0500

    se implemento el modelo y se corrigio el controlador también se modifico la tabla para que solo muestre los equipos en reserva y los que estan en estado Tramite y Autorizado

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,13 +3,9 @@
 class Conexion{
 
     static public function conectar(){
-<<<<<<< HEAD
         $link = new PDO("mysql:host=localhost;dbname=hermes002", "root", "");
 
         
-=======
-        $link = new PDO("mysql:host=localhost;dbname=hermes002", "root", "root");
->>>>>>> 0e26592232cd53c5e3bd9c58120100869179db01
         $link -> exec("set names utf8");
         return $link;
     }
