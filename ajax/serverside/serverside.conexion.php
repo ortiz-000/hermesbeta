f48f6cca8ce1e53d95a37f320237df8623281537
alojo2003 <alonsoarboleda17@gmail.com>
2025-06-06 15:23:18 -0500

    haciendo pull

--- ajax/serverside/serverside.conexion.php
+++ ajax/serverside/serverside.conexion.php
@@ -2,5 +2,5 @@
     define("HOST_SS", "localhost");
     define("USER_SS", "root");
     define("PASSWORD_SS", "");
-    define("DATABASE_SS", "solicitud");
+    define("DATABASE_SS", "hermes003");
 ?>
