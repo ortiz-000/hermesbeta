8c9b04cd8c2bcdee01ca3545cd360a219e6a57ff
German Ramirez <germanramirez@outlook.com>
2025-06-10 11:23:53 -0500

    chore: update configuration and database connection settings
    - Change RewriteBase in .htaccess from /proyecto/ to /hermesbeta/
    - Update database connection details in modelos/conexion.php and serverside.conexion.php
    - Remove duplicate 'estado' field from serverside.fichas.php query

--- ajax/serverside/serverside.fichas.php
+++ ajax/serverside/serverside.fichas.php
@@ -1,3 +1,3 @@
 <?php
     require 'serverside.php';
-    $table_data->get('vista_fichas_completa','id_ficha',array('id_ficha','codigo','descripcion_ficha','nombre_sede','fecha_inicio','fecha_fin','estado','id_sede','direccion_sede','duracion_dias','estado'));
+    $table_data->get('vista_fichas_completa','id_ficha',array('id_ficha','codigo','descripcion_ficha','nombre_sede','fecha_inicio','fecha_fin','estado','id_sede','direccion_sede','duracion_dias'));
