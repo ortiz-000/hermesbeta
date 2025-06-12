dc19490fc155418ff0d7e0b7de75946a287fdae9
Cristian Restrepo <restrepo5088@gmail.com>
2025-06-11 21:04:23 -0500

    fix : Se corrigue la eliminacion de los archivos de auditoria

--- ajax/auditoria.ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../controladores/auditoria.controlador.php";
-header('Content-Type: application/json');
-
-$idUsuario = isset($_GET['id_usuario']) ? (int)$_GET['id_usuario'] : null;
-
-$data = AuditoriaControlador::ctrMostrarAuditoria($idUsuario);
-
-echo json_encode(['data' => $data]);
