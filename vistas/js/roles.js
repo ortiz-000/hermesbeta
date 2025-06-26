436067dd7ff3f7c407b9d71a9bca744b6bb67483
Estebjack-2004git config --global user.name Estebjack-2004git config --global user.name Estebjack-2004 <ortizvasquezjuanesteban15@gmail.com>
2025-06-25 20:52:23 -0500

    se ha hecho la modidificacion en el s y el php de roles para las acciones y los estado

--- vistas/js/roles.js
+++ vistas/js/roles.js
@@ -1,101 +1,126 @@
-$(document).on("click", ".btnEditarRol", function() {
-    var idRol = $(this).attr("idRol");
-    var datos = new FormData();
-    datos.append("idRol", idRol);
-    $.ajax({
-        url: "ajax/roles.ajax.php",
-        method: "POST",
-        data: datos,
-        cache: false,
-        contentType: false,
-        processData: false,
-        dataType: "json",
-        success: function(respuesta) {
-            $("#nombreEditRol").val(respuesta["nombre_rol"]);
-            $("#descripcionEditRol").val(respuesta["descripcion"]);
-            $("#idEditRol").val(respuesta["id_rol"]);
+    /* ==================================================
+    SERVERSIDE ROLES
+    ================================================== */
 
-            if (parseInt(respuesta["id_rol"]) < 10) {
-                $("#nombreEditRol").prop("disabled", true);
-                $("#descripcionEditRol").prop("disabled", true);
-                $("#btnModificarRol").prop("disabled", true); 
-                if ($("#editRolWarning").length === 0) {
-                    $("#formEditRol").prepend('<div id="editRolWarning" class="alert alert-warning mt-2">La edición de este rol está bloqueada.</div>');
+    $('#tblRoles').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "ajax/serverside/serverside.roles.php",
+            "type": "POST"
+        },
+        "columns": [
+            { "data": null },
+            { "data": "nombre_rol" },
+            { "data": "descripcion" },
+            { "data": "fecha_creacion" },
+            { "data": "estado" },
+            { "data": null }
+        ],
+        "columnDefs": [
+            {
+                "targets": [0],
+                "render": function(data, type, row, meta) {
+                    return meta.row + 1;
                 }
-            } else {
-                $("#nombreEditRol").prop("disabled", false);
-                $("#descripcionEditRol").prop("disabled", false);
-                $("#btnModificarRol").prop("disabled", false); 
-                $("#editRolWarning").remove();
+            },
+            {
+                "targets": [4],
+                "render": function(data, type, row) {
+                    if (data === "activo") {
+                        return "<button class='btn btn-success btnActivarRol' idRol='" + row.id_rol + "' estadoRol='inactivo' title='Rol activo' data-toggle='tooltip'><i class='fas fa-check'></i></button>";
+                    } else {
+                        return "<button class='btn btn-danger btnActivarRol' idRol='" + row.id_rol + "' estadoRol='activo' title='Rol inactivo' data-toggle='tooltip'><i class='fas fa-ban'></i></button>";
+                    }
+                }
+            },
+            {
+                "targets": [-1],
+                "render": function(data, type, row) {
+                    return "<div class='btn-group'>" +
+                        "<button title='Consultar detalles' class='btn btn-default btnConsultarRol' idRol='" + row.id_rol + "' data-toggle='tooltip'><i class='fas fa-eye'></i></button>" +
+                        "<button title='Editar rol' class='btn btn-default btnEditarRol' idRol='" + row.id_rol + "' data-toggle='modal' data-target='#modalEditRol'><i class='fas fa-edit'></i></button>" +
+                        "<button title='Asignar permisos' class='btn btn-default btnPermisosRol' idRol='" + row.id_rol + "' data-toggle='tooltip'><i class='fas fa-key'></i></button>" +
+                    "</div>";
+                }
             }
-        }
+        ],
+        "responsive": true,
+        "autoWidth": false,
+        "lengthChange": true,
+        "lengthMenu": [10, 25, 50, 100],
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtrado de _MAX_ registros totales)",
+            "search": "Buscar:",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "buttons": ["csv", "excel", "pdf"],
+        "dom": "lfBrtip"
     });
-});
 
+    $(document).on("click", ".btnEditarRol", function() {
+        var idRol = $(this).attr("idRol");
+        var datos = new FormData();
+        datos.append("idRol", idRol);
+        
+        $.ajax({
+            url: "ajax/roles.ajax.php",
+            method: "POST",
+            data: datos,
+            cache: false,
+            contentType: false,
+            processData: false,
+            dataType: "json",
+            success: function(respuesta) {
+                $("#nombreEditRol").val(respuesta["nombre_rol"]);
+                $("#descripcionEditRol").val(respuesta["descripcion"]);
+                $("#idEditRol").val(respuesta["id_rol"]);
+            }
+        });
+    });
 
-$(document).on("click", ".btnActivarRol", function() {
-    var idRolActivar = $(this).attr("idRol");
-    var estadoRol = $(this).attr("estadoRol");
-    var datos = new FormData();
-    datos.append("idRolActivar", idRolActivar);
-    datos.append("estadoRol", estadoRol);
-    $.ajax({
-        url: "ajax/roles.ajax.php",
-        method: "POST",
-        data: datos,
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: function(respuesta) {
-            console.log("cambiado el estado", respuesta);
-        }
-    })
-    if (estadoRol == "inactivo") {
-        $(this).removeClass("btn-success");
-        $(this).addClass("btn-danger");
-        $(this).html('Inactivo');
-        $(this).attr("estadoRol", "activo");
-    }else {
-        $(this).removeClass("btn-danger");
-        $(this).addClass("btn-success");
-        $(this).html('Activo');
-        $(this).attr("estadoRol", "inactivo");
-    }
-});
+    $(document).on("click", ".btnActivarRol", function() {
+        var boton = $(this);
+        var idRolActivar = boton.attr("idRol");
+        var estadoRol = boton.attr("estadoRol");
+        var datos = new FormData();
+        datos.append("idRolActivar", idRolActivar);
+        datos.append("estadoRol", estadoRol);
 
-//tooltips
-$(document).ready(function () {
-    $('[data-bs-toggle="tooltip"]').tooltip();
-});
-$(document).on("click", ".btnEliminarRol", function() {
-    var idRol = $(this).attr("idRol");
-    Swal.fire({
-        title: '¿Estás seguro?',
-        text: "¡Esta acción no se puede deshacer!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, Eliminar el rol',
-        cancelButtonText: 'Cancelar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            var datos = new FormData();
-            datos.append("idRolEliminar", idRol);
-            $.ajax({
-                url: "ajax/roles.ajax.php",
-                method: "POST",
-                data: datos,
-                cache: false,
-                contentType: false,
-                processData: false,
-                success: function(respuesta) {
-                    Swal.fire('Eliminado', 'El rol ha sido eliminado y los usuarios asignados han sido inhabilitados.', 'success').then(()=>{
-
-                        location.reload();
-                    });
+        $.ajax({
+            url: "ajax/roles.ajax.php",
+            method: "POST",
+            data: datos,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(respuesta) {
+                if (estadoRol == "inactivo") {
+                    boton.removeClass('btn-success').addClass('btn-danger');
+                    boton.html('<i class="fas fa-ban"></i>');
+                    boton.attr("estadoRol", "activo");
+                    boton.attr("title", "Rol inactivo");
+                } else {
+                    boton.removeClass('btn-danger').addClass('btn-success');
+                    boton.html('<i class="fas fa-check"></i>');
+                    boton.attr("estadoRol", "inactivo");
+                    boton.attr("title", "Rol activo");
                 }
-            });
-        }
+                boton.tooltip('hide').tooltip('show');
+            }
+        });
     });
-});
+
+    // Tooltips
+    $('#tblRoles').on('draw.dt', function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
