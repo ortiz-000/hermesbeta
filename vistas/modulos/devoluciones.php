1ff251838c871cc65e660e426fa5cfac18895df1
Cristian Gomez <ycamigo123@gmail.com>
2025-06-29 15:37:33 -0500

    refactor(devoluciones): remove unused buttons from loan details view
    Simplify the interface by removing edit and related requests buttons that were not being used in the loan details view. The view button remains as the primary action.

--- vistas/modulos/devoluciones.php
+++ vistas/modulos/devoluciones.php
@@ -62,16 +62,10 @@
                       <td>' . $value["fecha_inicio"] . '</td>
                       <td>' . $value["fecha_fin"] . '</td>
                       <td>' . $value["tipo_prestamo"] . '</td>
-                      <td>
+                      <td class="text-center">
                         <div class="btn-group">
                             <button title="Consultar detalles de préstamo" class="btn btn-default btn-sm btnVerUsuario" data-id="' . $value["id_prestamo"] . '" data-toggle="modal" data-target="#modalVerDetallesPrestamo">
                                 <i class="fas fa-eye"></i>
-                            </button>
-                            <button title="Editar préstamo" class="btn btn-default btn-sm btnEditarPrestamo" data-id="' . $value["id_prestamo"] . '" data-toggle="modal" data-target="#modalEditarPrestamo">
-                                <i class="fas fa-edit"></i>
-                            </button>
-                            <button title="Solicitudes relacionadas" class="btn btn-default btn-sm btnSolicitudesPrestamo" data-id="' . $value["id_prestamo"] . '" data-toggle="modal" data-target="#modalSolicitudesPrestamo">
-                                <i class="fas fa-laptop"></i>
                             </button>
                         </div>
                       </td>
