687944fb1ccf6f95447f8e2f01ba6d136318a7d6
German Ramirez <germanramirez@outlook.com>
2025-07-06 13:15:15 -0500

    Refactor login page: update logo display, change sign-in message to brand name, and enhance button styling for improved user experience.

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -1,11 +1,13 @@
   <div class="login-box">
     <div class="login-logo">
-      <a href="#">HERMES</a>
+      <img src="vistas/img/Logo/android-chrome-192x192.png" alt="HERMES Logo" class="img-fluid mb-3">
+      <br>    
+      <!-- <a href="#">HERMES</a> -->
     </div>
     <!-- /.login-logo -->
     <div class="card">
       <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign in to start your session</p>
+        <p class="login-box-msg">HERMES</p>
 
         <form action="" method="post">
           <div class="input-group mb-3">
@@ -25,17 +27,17 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-8">
+            <!-- <div class="col-8">
               <div class="icheck-primary">
                 <input type="checkbox" id="remember">
                 <label for="remember">
                   Recuerdame
                 </label>
               </div>
-            </div>
+            </div> -->
             <!-- /.col -->
-            <div class="col-4">
-              <button type="submit" class="btn btn-primary" id="ingresar">Ingresar</button>
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary btn-block" id="ingresar">Ingresar</button>
             </div>
             <!-- /.col -->
           </div>
@@ -47,9 +49,9 @@
 
         </form>
 
-        <p class="mb-1">
+        <!-- <p class="mb-1">
           <a href="forgot-password.html">I forgot my password</a>
-        </p>
+        </p> -->
       </div>
       <!-- /.login-card-body -->
     </div>
