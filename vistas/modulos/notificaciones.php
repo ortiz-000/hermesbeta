6da530b912b0adb0811a657ef080871cea86c995
Nicolas <jperezmillan8@gmail.com>
2025-07-07 14:33:12 -0500

    refactor(notifications): remove unused UI elements from notifications module
    Clean up the notifications interface by removing redundant trash, reply, share and refresh buttons, as well as unused tags section to simplify the UI and improve maintainability

--- vistas/modulos/notificaciones.php
+++ vistas/modulos/notificaciones.php
@@ -87,50 +87,10 @@
                     <span class="badge bg-primary float-right"><?php echo $totalNoLeidas; ?></span>
                   </a>
                 </li>  
-                <li class="nav-item">
-                  <a href="#" class="nav-link">
-                    <i class="far fa-trash-alt"></i> Papelera
-                  </a>
-                </li>
               </ul>
             </div>
             <!-- /.card-body -->
           </div>
-          <!-- /.card -->
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Etiquetas</h3>
-
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-              </div>
-            </div>
-            <div class="card-body p-0">
-              <ul class="nav nav-pills flex-column">
-                <li class="nav-item">
-                  <a href="#" class="nav-link">
-                    <i class="far fa-circle text-danger"></i>
-                    Importante
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="#" class="nav-link">
-                    <i class="far fa-circle text-warning"></i> Advertencia
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="#" class="nav-link">
-                    <i class="far fa-circle text-primary"></i>
-                    Completado
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
         </div>
         <!-- /.col -->
         <div class="col-md-9">
@@ -160,17 +120,8 @@
                   <button type="button" class="btn btn-default btn-sm btn-delete-selected">
                     <i class="far fa-trash-alt"></i>
                   </button>
-                  <button type="button" class="btn btn-default btn-sm">
-                    <i class="fas fa-reply"></i>
-                  </button>
-                  <button type="button" class="btn btn-default btn-sm">
-                    <i class="fas fa-share"></i>
-                  </button>
                 </div>
                 <!-- /.btn-group -->
-                <button type="button" class="btn btn-default btn-sm">
-                  <i class="fas fa-sync-alt"></i>
-                </button>
                 <div class="float-right">
                   1-50/200
                   <div class="btn-group">
@@ -248,17 +199,8 @@
                 <button type="button" class="btn btn-default btn-sm btn-delete-selected">
                   <i class="far fa-trash-alt"></i>
                 </button>
-                  <button type="button" class="btn btn-default btn-sm">
-                    <i class="fas fa-reply"></i>
-                  </button>
-                  <button type="button" class="btn btn-default btn-sm">
-                    <i class="fas fa-share"></i>
-                  </button>
                 </div>
                 <!-- /.btn-group -->
-                <button type="button" class="btn btn-default btn-sm">
-                  <i class="fas fa-sync-alt"></i>
-                </button>
                 <div class="float-right">
                   1-50/200
                   <div class="btn-group">
