929d341d9ebccc54be0d542065e4429bb121fcc5
German Ramirez <germanramirez@outlook.com>
2025-07-13 10:58:31 -0500

    permissions fichas module

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -77,7 +77,7 @@
             </a>
           </li>';
 
-        if (ControladorValidacion::validarPermisoSesion([10])) {
+        if (ControladorValidacion::validarPermisoSesion([22,25,28,30,29])) {
           echo '<li class="nav-item">
                       <a href="#" class="nav-link">
                       <i class="nav-icon fas fa-cogs"></i>
