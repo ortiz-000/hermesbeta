dd42f75f5e9d89dd6319732e75371099d4b097bd
Cristian Gomez <ycamigo123@gmail.com>
2025-07-17 15:37:35 -0500

    style(about): update card styling and developer info in about page
    - Change card-info to card-primary for better visual distinction
    - Update callout classes to callout-success for consistency
    - Add missing social media links for developers
    - Improve support hours section with additional details

--- vistas/modulos/acerca-de.php
+++ vistas/modulos/acerca-de.php
@@ -16,7 +16,7 @@
             <!-- Sobre el proyecto -->
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card card-info card-outline">
+                    <div class="card card-info card-primary">
                         <div class="card-header">
                             <h3 class="card-title"><i class="fas fa-info-circle mr-2"></i>¿Qué es HERMES?</h3>
                             <div class="card-tools">
@@ -26,11 +26,13 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <div class="callout callout-info">
+                            <div class="callout callout-success">
                                 <p><strong>HERMES</strong> (Herramientas y Equipos Reservables para el Manejo Estratégico de Servicios) es un sistema interno diseñado para apoyar la formación en el <strong>SENA</strong>, facilitando el acceso organizado y responsable a los recursos necesarios en las prácticas formativas.</p>
                             </div>
 
-                            <p>Permite gestionar el préstamo de herramientas, equipos y materiales utilizados en el desarrollo de actividades académicas, mostrando su disponibilidad, estado de préstamo y las normas de uso establecidas.</p>
+                            <div class="callout callout-success">
+                                <p>Permite gestionar el préstamo de herramientas, equipos y materiales utilizados en el desarrollo de actividades académicas, mostrando su disponibilidad, estado de préstamo y las normas de uso establecidas.</p>
+                            </div>
 
                             <div class="row mt-4">
                                 <div class="col-md-6">
@@ -65,9 +67,13 @@
                             </div>
 
                             <div class="alert alert-info mt-4">
-                                <h5><i class="icon fas fa-clock mr-2"></i>Horarios de atención</h5>
+                                <h5><i class="icon fas fa-clock mr-2"></i>Horarios de atención de soporte de software</h5>
                                 <p class="mb-0">Lunes a viernes: 7:00 a.m. – 12:00 m. | 1:00 p.m. – 5:00 p.m.</p>
+                                <p class="mb-0">Oficina Mesa de Ayuda - Siempre dispuestos a mejorar con el apoyo de nuevos talentos técnicos.</p>
+                                <p class="mb-0" style="text-decoration: underline;">*Promovemos el fortalecimiento del equipo técnico mediante la integración de aprendices SENA en procesos clave de soporte*</p>
                             </div>
+
+
                         </div>
                     </div>
                 </div>
@@ -92,20 +98,20 @@
                                     ["nombre" => "German Ramirez Ramirez", "rol" => "Product Owner", "foto" => "vistas/img/desarrolladores/grr.jpg", "github" => "https://github.com/GermanRz", "instagram" => "https://www.instagram.com/germanrzrz?igsh=MW56cngyNjNvNWw1NQ== "],
                                     ["nombre" => "Cristian Camilo Gomez Gonzalez", "rol" => "Líder del módulo de Devoluciones", "foto" => "vistas/img/desarrolladores/ccgg.jpg", "github" => "https://github.com/PatoTaactico", "instagram" => "https://www.instagram.com/criz._.ggku/"],
                                     ["nombre" => "Jhoan David Sinisterra Valencia", "rol" => "Líder del módulo de Equipos", "foto" => "vistas/img/desarrolladores/jdsv.jpg", "github" => "https://github.com/Davidjdsv", "instagram" => "https://www.instagram.com/davidjdsv_2509/"],
-                                    ["nombre" => "Juan Diego Millán Arango", "rol" => "Líder del módulo de Usuarios", "foto" => "vistas/img/desarrolladores/jdma.jpg", "github" => "https://github.com/JUANDMILLAN", "instagram" => ""],
+                                    ["nombre" => "Juan Diego Millán Arango", "rol" => "Líder del módulo de Usuarios", "foto" => "vistas/img/desarrolladores/jdma.jpg", "github" => "https://github.com/JUANDMILLAN", "instagram" => "https://www.instagram.com/arango_juan11?igsh=MXV4ZnR1ZDMxb2JnNQ%3D%3D&utm_source=qr"],
                                     ["nombre" => "Brayan Camilo Ospina Gonzalez", "rol" => "Líder del módulo de Autorizaciones", "foto" => "vistas/img/desarrolladores/bcog.jpg", "github" => "https://github.com/Br4nnnn", "instagram" => "https://www.instagram.com/bra.n_/"],
-                                    ["nombre" => "Alonso Arboleda Obando", "rol" => "Líder del módulo de Solicitudes", "foto" => "vistas/img/desarrolladores/aao.jpg", "github" => "https://github.com/alojoarboleda2003", "instagram" => ""],
+                                    ["nombre" => "Alonso Arboleda Obando", "rol" => "Líder del módulo de Solicitudes", "foto" => "vistas/img/desarrolladores/aao.jpg", "github" => "https://github.com/alojoarboleda2003", "instagram" => "https://www.instagram.com/alonsoarboledaobando?igsh=ZWNxcmV6YWVwZzY3"],
                                     ["nombre" => "Jack Esteban Ortiz Vásquez", "rol" => "Módulo de Autorizaciones", "foto" => "vistas/img/desarrolladores/jeov.jpg", "github" => "https://github.com/Estebjack-2004", "instagram" => "https://www.instagram.com/jack_raccoon?igsh=NnR2c2kzYjJqbjE= "],
                                     ["nombre" => "Nicolás Manzano Muriel", "rol" => "Módulo de Autorizaciones", "foto" => "vistas/img/desarrolladores/nmm.jpg", "github" => "https://github.com/Toizomo", "instagram" => "https://www.instagram.com/nic_romance/"],
-                                    ["nombre" => "Alejandro Lozada Vera", "rol" => "Módulo de Usuarios", "foto" => "vistas/img/desarrolladores/alv.jpg", "github" => "https://github.com/alejandrovera14", "instagram" => ""],
+                                    ["nombre" => "Alejandro Lozada Vera", "rol" => "Módulo de Usuarios", "foto" => "vistas/img/desarrolladores/alv.jpg", "github" => "https://github.com/alejandrovera14", "instagram" => "https://www.instagram.com/alejo.vera2?igsh=d2dma2lqMXBudHZq&utm_source=qr"],
                                     ["nombre" => "Santiago Franco Flórez", "rol" => "Módulo de Solicitudes", "foto" => "vistas/img/desarrolladores/sff.jpg", "github" => "https://github.com/sanfranco2002", "instagram" => "https://www.instagram.com/santi_franco2002?igsh=aWloejBlNTNoMjEw "],
                                     ["nombre" => "David Satizábal", "rol" => "Módulo de Equipos", "foto" => "vistas/img/desarrolladores/ds.jpg", "github" => "https://github.com/Davidsatizabal", "instagram" => "https://www.instagram.com/david.satizabal?igsh=ZGtjbGFudmFjamwx "],
                                     ["nombre" => "Juan Sebastián Velásquez Ortiz", "rol" => "Módulo de Salidas", "foto" => "vistas/img/desarrolladores/jsvo.jpg", "github" => "https://github.com/ortiz-000", "instagram" => ""],
-                                    ["nombre" => "Jensen Ballén Banguera", "rol" => "Módulo de Solicitudes", "foto" => "vistas/img/desarrolladores/jbb.jpg", "github" => "https://github.com/jensen-ballen", "instagram" => ""],
-                                    ["nombre" => "Juan Pablo Montaño Pérez", "rol" => "Módulo de Equipos", "foto" => "vistas/img/desarrolladores/jpmp.jpg", "github" => "https://github.com/JuanPablo-mp", "instagram" => ""],
-                                    ["nombre" => "Karen Vanessa Valencia Grueso", "rol" => "Módulo de Salidas", "foto" => "vistas/img/desarrolladores/kvvg.jpg", "github" => "https://github.com/KARENVALENC", "instagram" => "https://www.instagram.com/karen_valenciakv?igsh=ZXRhNzd1cWNzZGUy "],
-                                    ["nombre" => "Jhon Edison Díaz Ruiz", "rol" => "Módulo de Mantenimiento", "foto" => "vistas/img/desarrolladores/jedr.jpg", "github" => "https://github.com/Jediaz23", "instagram" => ""],
-                                    ["nombre" => "Cristian Camilo Restrepo Muriel", "rol" => "Módulo de Equipos", "foto" => "vistas/img/desarrolladores/ccrm.jpg", "github" => "https://github.com/CamiloRestre", "instagram" => ""]
+                                    ["nombre" => "Jensen Ballén Banguera", "rol" => "Módulo de Solicitudes", "foto" => "vistas/img/desarrolladores/jbb.jpg", "github" => "https://github.com/jensen-ballen", "instagram" => "https://www.instagram.com/jensenballen?igsh=MTRpcHBua2luMWd0cw=="],
+                                    ["nombre" => "Juan Pablo Montaño Pérez", "rol" => "Módulo de Equipos", "foto" => "vistas/img/desarrolladores/jpmp.jpg", "github" => "https://github.com/JuanPablo-mp", "instagram" => "https://www.instagram.com/juanpa_13mp?igsh=bWZodG1xcm1rZzVx"],
+                                    ["nombre" => "Jhon Edison Díaz Ruiz", "rol" => "Módulo de Mantenimiento", "foto" => "vistas/img/desarrolladores/jedr.jpg", "github" => "https://github.com/Jediaz23", "instagram" => "https://www.instagram.com/jhoon_dxz00?igsh=bW9sc2JqYThkNWE5"],
+                                    ["nombre" => "Cristian Camilo Restrepo Muriel", "rol" => "Módulo de Equipos", "foto" => "vistas/img/desarrolladores/ccrm.jpg", "github" => "https://github.com/CamiloRestre", "instagram" => "https://www.instagram.com/camilo_restre?igsh=MWZkbTQ0bnRqeXVqeg=="],
+                                    ["nombre" => "Karen Vanessa Valencia Grueso", "rol" => "Módulo de Salidas", "foto" => "vistas/img/desarrolladores/kvvg.jpg", "github" => "https://github.com/KARENVALENC", "instagram" => "https://www.instagram.com/karen_valenciakv?igsh=ZXRhNzd1cWNzZGUy "]
                                 ];
 
                                 foreach ($desarrolladores as $dev) {
@@ -113,7 +119,7 @@
                                     $rutaRelativa = $dev["foto"];
                                     $rutaAbsoluta = $_SERVER["DOCUMENT_ROOT"] . "/hermesbeta/" . $dev["foto"];
                                     $foto = file_exists($rutaAbsoluta) ? $rutaRelativa : "vistas/img/usuarios/default/anonymous.jpg";
-                                    
+
                                     $githubLink = isset($dev["github"]) ? $dev["github"] : "#";
                                     $instagramLink = isset($dev["instagram"]) ? $dev["instagram"] : "#";
 
